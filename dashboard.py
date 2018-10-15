d62973e4eacb8768fb7e22b9da2620fd300b37d3
Cnels1986 <CNels1986@gmail.com>
2018-10-15 08:37:40 -0400

    total profit calculated

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import json
+from re import sub
+from decimal import Decimal
 
 with open('sales_staff.json', 'r') as staffFile:
     staff = json.loads(staffFile.read())
@@ -11,6 +13,24 @@ with open('sales.json', 'r') as salesFile:
     sales = json.loads(salesFile.read())
     # print(sales)
 
+personCount = 1
+
+# for person in staff:
+#     carsSold = 0
+#     salesAmount = 0
+#     for sale in sales:
+#         if(sale['salesperson'] == personCount):
+#             carsSold += 1
+#             salesAmount +=
+#     print(carsSold)
+#
+
+
+# finds the total profit made from the dataset
+total_profit = 0
 for sale in sales:
-    print(sale)
-    print("--------")
+    base_price = Decimal(sub(r'[^\d.]', '', sale['base_price']))
+    accessories = Decimal(sub(r'[^\d.]', '', sale['accessories']))
+    taxes = Decimal(sub(r'[^\d.]', '', sale['taxes']))
+    total_profit += (base_price + accessories) - taxes
+print('${:,.2f}'.format(total_profit))
