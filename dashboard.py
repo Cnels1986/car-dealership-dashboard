6da924d6eb7401032301f42b39d38106e84c27eb
Cnels1986 <CNels1986@gmail.com>
2018-10-15 08:52:31 -0400

    number of cars sold by each sales person and the amount of profit they each earned has be calculated

--- dashboard.py
+++ dashboard.py
@@ -15,22 +15,33 @@ with open('sales.json', 'r') as salesFile:
 
 personCount = 1
 
-# for person in staff:
-#     carsSold = 0
-#     salesAmount = 0
-#     for sale in sales:
-#         if(sale['salesperson'] == personCount):
-#             carsSold += 1
-#             salesAmount +=
-#     print(carsSold)
-#
+for person in staff:
+    carsSold = 0
+    salesAmount = 0
+    for sale in sales:
+        if(sale['salesperson'] == personCount):
+
+            base_price = Decimal(sub(r'[^\d.]', '', sale['base_price']))
+            accessories = Decimal(sub(r'[^\d.]', '', sale['accessories']))
+            taxes = Decimal(sub(r'[^\d.]', '', sale['taxes']))
+
+            carsSold += 1   #increments cars sold
+            salesAmount += (base_price + accessories) - taxes   #adds the amount made after taxes
+    print(carsSold)
+    print(type(salesAmount))
+    # commission = salesAmount * person['commission']
+    # print(commission)
+    print(type(person['commission']))
+    print("========")
+    personCount += 1
+
 
 
 # finds the total profit made from the dataset
-total_profit = 0
-for sale in sales:
-    base_price = Decimal(sub(r'[^\d.]', '', sale['base_price']))
-    accessories = Decimal(sub(r'[^\d.]', '', sale['accessories']))
-    taxes = Decimal(sub(r'[^\d.]', '', sale['taxes']))
-    total_profit += (base_price + accessories) - taxes
-print('${:,.2f}'.format(total_profit))
+# total_profit = 0
+# for sale in sales:
+#     base_price = Decimal(sub(r'[^\d.]', '', sale['base_price']))
+#     accessories = Decimal(sub(r'[^\d.]', '', sale['accessories']))
+#     taxes = Decimal(sub(r'[^\d.]', '', sale['taxes']))
+#     total_profit += (base_price + accessories) - taxes
+# print('${:,.2f}'.format(total_profit))
