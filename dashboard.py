0a412fbdaa798a621f47d6f3e12aa197459b0119
Cnels1986 <CNels1986@gmail.com>
2018-10-16 08:36:30 -0400

    sales person table is complete within the html template of the dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
+from flask import Flask
+from flask import render_template
 import matplotlib
 import numpy as np
 import json
@@ -6,14 +8,18 @@ from re import sub
 from decimal import Decimal
 import math
 import operator
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def index():
+#     return render_template('dashboard.html', profit=total_profit, tableData = sorted_chartData)
 
 with open('sales_staff.json', 'r') as staffFile:
     staff = json.loads(staffFile.read())
-    # print(staff)
 
 with open('sales.json', 'r') as salesFile:
     sales = json.loads(salesFile.read())
-    # print(sales)
 
 personCount = 1
 chartData = []
@@ -32,9 +38,32 @@ for person in staff:
             carsSold += 1   #increments cars sold
             salesAmount += (base_price + accessories) - taxes   #adds the amount made after taxes
     commission = float(salesAmount) * person['commission']
-    dict = {'first': person['first_name'], 'last': person['last_name'], 'carsSold': carsSold, 'saleAmount': float(salesAmount), 'commission': round(commission,2)}
+    name = person['first_name'] + " " + person['last_name']
+    dict = {'name': name, 'carsSold': carsSold, 'saleAmount': '${:,.2f}'.format(float(salesAmount)), 'commission': '${:,.2f}'.format(round(commission,2))}
     chartData.append(dict)
     personCount += 1
+
+sorted_chartData = sorted(chartData, key=operator.itemgetter('carsSold'), reverse=True)
+# print(sorted_chartData)
+salesRankingSold = {}
+salesRankingAmount = {}
+salesRankingCommission = {}
+
+for rank in sorted_chartData:
+    names = rank['name']
+    salesRankingSold[name] = rank['carsSold']
+    salesRankingAmount[name] = rank['saleAmount']
+    salesRankingCommission[name] = rank['commission']
+
+names = list(salesRankingSold.keys())
+valuesSold = list(salesRankingSold.values())
+valuesAmount = list(salesRankingAmount.values())
+valuesCommission = list(salesRankingCommission.values())
+# print(names)
+# print(valuesSold)
+# print(valuesAmount)
+# print(valuesCommission)
+
 
 models = {}
 makes = {}
@@ -61,6 +90,40 @@ for sale in sales:
     else:
         models[model] = 1
 
+total_profit = '${:,.2f}'.format(total_profit)
+# print(total_profit)
+# creates sorted lists of the dictionaries, list contains tuples and is sorted by values
 makesSorted = sorted(makes.items(), key=operator.itemgetter(1), reverse=True)
 modelsSorted = sorted(models.items(), key=operator.itemgetter(1), reverse=True)
+top10makes = makesSorted[0:10]
 top10models = modelsSorted[0:10]
+
+name = []
+values = []
+for model in top10models:
+    name.append(model[0])
+    values.append(model[1])
+fig, axs = plt.subplots()
+axs.bar(name, values)
+axs.set_ylabel('Number Sold')
+axs.set_xlabel('Models')
+axs.set_title('Top 10 Models Sold')
+plt.xticks(rotation=45)
+fig.tight_layout()
+# mpld3.show()
+fig1 = mpld3.fig_to_html(fig)
+print(fig1)
+
+# name1 = []
+# values1 = []
+# for make in top10makes:
+#     name1.append(make[0])
+#     values1.append(make[1])
+# fig, axs = plt.subplots()
+# axs.bar(name, values)
+# axs.set_ylabel('Number Sold')
+# axs.set_xlabel('Makes')
+# axs.set_title('Top 10 Makes Sold')
+# plt.xticks(rotation=45)
+# fig.tight_layout()
+# mpld3.show()
